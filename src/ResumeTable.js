1c3d20bd527aa0fcd8edcfa72697da484cb86793
Mitul Shah <mitulshah97@gmail.com>
2021-04-30 17:48:05 -0400

    get new items from database

--- src/ResumeTable.js
+++ src/ResumeTable.js
@@ -5,22 +5,33 @@ import 'react-super-responsive-table/dist/SuperResponsiveTableStyle.css';
 //import { BiEdit } from 'react-icons/bi';
 
 const ResumeTable = ({records}) => {
+  
   return (
     <Table>
       <Thead>
         <Tr>
           <Th>First Name</Th>
           <Th>Last Name</Th>
+          <Th>Phone</Th>
+          <Th>Email</Th>
+          <Th>Skills</Th>
           <Th>Resume</Th>
           <Th>Edit</Th>
         </Tr>
       </Thead>
       <Tbody>
         {records.map((record) => {
-        const { id, fname, lname, resume } = record
+        
+        const { id, fname, lname, phone, email, skills, resume } = record
+        
         return  <Tr key={id}>
                 <Td component="th" scope="row">{fname}</Td>
                 <Td>{lname}</Td>
+                <Td>{phone}</Td>
+                <Td>{email}</Td>
+
+                <Td>{skills && skills.map(item => <p key={Math.random()}>{item}</p>)}</Td>
+                
                 <Td><a className='link' href={resume} target="_blank">{resume}</a></Td>
                 <Td>
                   <Link to={`/${id}`}>
